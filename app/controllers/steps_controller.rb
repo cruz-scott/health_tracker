6de8e27b4692384d61c20c8c2b43d68da55e1031
Cruz <nunez.a.cruz@gmail.com>
2015-06-04 15:07:09 -0400

    Add functionality to StepsControler

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,4 +1,7 @@
 class StepsController < ApplicationController
+  before_action :set_steps, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @steps = Step.all
   end
@@ -14,11 +17,48 @@ class StepsController < ApplicationController
   end
 
   def create
+    @step = Step.new(step_params)
+
+    respond_to do |format|
+      if @step.save
+        format.html { redirect_to @step, notice: 'Weight was successfully created.' }
+        format.json { render :show, status: :created, location: @step }
+      else
+        format.html { render :new }
+        format.json { render json: @step.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @step.update(step_params)
+        format.html { redirect_to @step, notice: 'Weight was successfully updated.' }
+        format.json { render :show, status: :ok, location: @step }
+      else
+        format.html { render :edit }
+        format.json { render json: @step.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @step.destroy
+    respond_to do |format|
+      format.html { redirect_to steps_url, notice: 'Weight was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_steps
+      @step = Step.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def step_params
+      params.require(:step).permit(:value)
+    end
+
 end
