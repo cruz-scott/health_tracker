eb21a84b341a3dbf88db462e6254447d7c1eae31
Cruz <nunez.a.cruz@gmail.com>
2015-06-05 15:16:41 -0400

    Validate weight once per day & fix corresponding texts

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,2 +1,13 @@
 class Weight < ActiveRecord::Base
+  validate :same_date, on: :create
+
+  private def same_date
+    if Weight.count == 0
+      true
+    else
+      if Time.now.strftime("%D") == Weight.last.created_at.strftime("%D")
+        errors.add(:_, "Cannot weigh yourself in twice on same day")
+      end
+    end
+  end
 end
