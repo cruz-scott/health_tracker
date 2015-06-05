27fddc3db0d00cc765ddfa0e427efb72dbf6a062
Cruz <nunez.a.cruz@gmail.com>
2015-06-05 14:03:32 -0400

    Add first feature of our choosing: net pounds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def index
     @calories = Calory.all
     @Exercises = Exercise.all
+    @calories_ate = Calory.calories_today
+    @calories_burned = Exercise.calories_burned_today
   end
 
 end
