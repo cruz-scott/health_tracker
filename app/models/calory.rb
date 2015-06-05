fb85268646c6716b0c05580e19d2559089d1f9c5
Cruz <nunez.a.cruz@gmail.com>
2015-06-05 12:23:26 -0400

    Fix dashboard if exercises table and calories table are blank

--- app/models/calory.rb
+++ app/models/calory.rb
@@ -1,9 +1,13 @@
 class Calory < ActiveRecord::Base
 
   def self.calories_today
-    calories_today = Calory.order(:value).map {|c| c.value if c.created_at.day == Time.now.day}
-    calories_today.reject! {|c| c == nil}
-    calories_today.reduce(:+)
+    if Calory.count == 0
+      0
+    else
+      calories_today = Calory.order(:value).map {|c| c.value if c.created_at.day == Time.now.day}
+      calories_today.reject! {|c| c == nil}
+      calories_today.reduce(:+)
+    end
   end
 
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,8 +2,12 @@ class Exercise < ActiveRecord::Base
   belongs_to :exercise_type
 
   def self.calories_burned_today
-    calories_today = Exercise.order(:calories_burned).map {|c| c.calories_burned if c.created_at.day == Time.now.day}
-    calories_today.reject! {|c| c == nil}
-    calories_today.reduce(:+) 
+    if Exercise.count == 0
+      0
+    else
+      calories_today = Exercise.order(:calories_burned).map {|c| c.calories_burned if c.created_at.day == Time.now.day}
+      calories_today.reject! {|c| c == nil}
+      calories_today.reduce(:+)
+    end
   end
 end
