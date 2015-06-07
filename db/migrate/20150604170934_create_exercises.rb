ff784ec7253753ab497017cb755b251a9eb409cb
Scott Williams <williamsjs1224@gmail.com>
2015-06-07 17:15:46 -0400

    add foreigner gem

--- db/migrate/20150604170934_create_exercises.rb
+++ db/migrate/20150604170934_create_exercises.rb
@@ -2,6 +2,7 @@ class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
       t.references :exercise_type, index: true, foreign_key: true
+      t.foreign_key :exercise_typed
       t.integer :calories_burned
 
       t.timestamps null: false
